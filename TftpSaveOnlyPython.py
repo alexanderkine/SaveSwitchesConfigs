9a53e47bccbe6e823d47ce020f86e3e1e98867e9
alexanderkine <sanya_179510@mail.ru>
2025-04-04 03:16:57 +0500

    Allocated a separate method for connecting to switches, refactored the code and made the path to the file with the IP of the switches enter

--- TftpSaveOnlyPython.py
+++ TftpSaveOnlyPython.py
@@ -10,22 +10,15 @@ import os
 # Configuration options
 print('Конфигурации сохраняются в папке вида CiscoBackups_<date>\n')
 print('Введите абсолютный путь сохранения конфигураций (пустое поле означает сохранение по текущему месту скрипта) и нажмите <Enter>:')
-
 backup_path = input().strip()
-symbol = "" if backup_path == "" else "/"
-current_date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-folder_path = f"{backup_path}{symbol}CiscoBackups_{current_date}".strip()
-os.mkdir(folder_path)
-stations = ('Динамо', 'Уральская', 'Машиностроителей', 'Уралмаш', 'Проспект Космонавтов', 'Площадь 1905 года', 'Геологическая', 'Депо', 'Инженерный корпус', 'Чкаловская', 'Ботаническая')   
-
-for station in stations:
-        os.mkdir(f'{folder_path}\{station}')
+print('Введите абсолютный путь к файлу с IP коммутаторов (пустое поле означает файл c именем devices по текущему месту скрипта) и нажмите <Enter>:')
+devices_path = input().strip()
 
 def parse_devices_file(path):
     devices_file = open(path, "r")
     devices = []
     while True:
-        ip = devices.readline().strip()
+        ip = devices_file.readline().strip()
         if not ip:
             break
         devices.append(ip)
@@ -33,8 +26,18 @@ def parse_devices_file(path):
 
     return devices
 
-def save_config_from_device(station_folder, ip):
-    device =  { 'device_type':'cisco_ios_telnet', 'ip':ip, 'username':'root', 'password':'root', 'port': 23}
+stations = ('Динамо', 'Уральская', 'Машиностроителей', 'Уралмаш', 'Проспект Космонавтов', 'Площадь 1905 года', 'Геологическая', 'Депо', 'Инженерный корпус', 'Чкаловская', 'Ботаническая')   
+devices_file = parse_devices_file('devices' if devices_path == '' else f'{devices_path}')
+symbol = "" if backup_path == "" else "/"
+current_date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+folder_path = f"{backup_path}{symbol}CiscoBackups_{current_date}".strip()
+os.mkdir(folder_path)
+os.chdir(folder_path)
+
+for station in stations:
+        os.mkdir(f'{station}')
+
+def connect_and_save_output(device):
     try:
         net_connect = ConnectHandler(**device)
     except:
@@ -44,17 +47,21 @@ def save_config_from_device(station_folder, ip):
             net_connect.enable()
             hostname = net_connect.find_prompt().strip('#')
             config_output = net_connect.send_command('show running-config')
-    
-            # сохранение конфигурации в файл
-            with open(f'{folder_path}\{station_folder}\{ip} ({hostname}_backup_{current_date}).txt', 'w') as file:
-                file.write(config_output)
-        
-            print(f'{station_folder} {ip} успешно сохранено!')
             net_connect.disconnect()
 
-for ip in parse_devices_file(f"devices"):	
+    return (config_output, hostname)
+
+def save_config_from_device(station_folder, ip):
+    device =  { 'device_type':'cisco_ios_telnet', 'ip':ip, 'username':'root', 'password':'root', 'port': 23}
+    output = connect_and_save_output(device)
+    # сохранение конфигурации в файл
+    with open(f'{station_folder}\{ip} ({output[1]}_backup_{current_date}.txt', 'w') as file:
+        file.write(output[0])
+        
+    print(f'{station_folder} {ip} успешно сохранено!')
+
+for ip in devices_file:	
     station_number = ip.split('.')[1]
     station_folder = stations[int(station_number) - 1]
-
-save_config_from_device(station_folder, ip)
+    save_config_from_device(station_folder, ip)
 
